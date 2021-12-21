9c86214552b52eea42b0060e62ea06ce41f0e44d
whatanakKean <85047164+whatanakKean@users.noreply.github.com>
2021-12-21 15:57:22 +0700

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+//author: Kean Whatanak
+
+
+int main()
+{
+    int input = -1;
+    cout<<"Menu: \n";
+    while(input < 1 || input > 6)
+    {
+        cout<<"\t 1.Display student information.\n";
+        cout<<"\t 2.Add student into list\n";
+        cout<<"\t 3.Remove specific Student\n";
+        cout<<"\t 4.Update student informtion\n";
+        cout<<"\t 5.Delete all students\n";
+        cout<<"\t 6.Display operation history\n";
+        cin>>input;
+    }
+
+    if(input == 1)
+    {
+
+    }
+    else if(input == 2)
+    {
+
+    }
+    else if(input == 3)
+    {
+
+    }
+    else if(input == 4)
+    {
+
+    }
+    else if(input == 5)
+    {
+
+    }
+    else if(input == 6)
+    {
+
+    }
+
+    return 0;
+}
